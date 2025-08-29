a33272e2e8a9d4ec97b1ba45b2eadeeef0681c4d
Madhuri_N <madhuri.nekkanti@caprusit.com>
2025-08-29 13:21:08 +0530

    nested elements

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello from React");
+// <div id="parent">
+//     <div id="child">
+//       <h1>I'am h1 tag</h1>
+//     </div>
+// </div>
+// ReactElement(object) => HTML(Browser Understands)
+
+// for creating nested elements
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "heading-h1 tag")
+  )
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
-
+root.render(heading);
