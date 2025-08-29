fc32df5b71eadc9beb94558491e381a7bc950fc1
Madhuri_N <madhuri.nekkanti@caprusit.com>
2025-08-29 13:24:55 +0530

    nested siblings

--- App.js
+++ App.js
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom/client";
 // <div id="parent">
 //     <div id="child">
 //       <h1>I'am h1 tag</h1>
+//       <h2>I'am h2 tag</h1>
 //     </div>
 // </div>
 // ReactElement(object) => HTML(Browser Understands)
 
 // for creating nested elements
+
+// siblings - use Array
 
 const heading = React.createElement(
   "div",
@@ -16,7 +19,8 @@ const heading = React.createElement(
   React.createElement(
     "div",
     { id: "child" },
-    React.createElement("h1", {}, "heading-h1 tag")
+    [React.createElement("h1", {}, "heading-h1 tag"),
+     React.createElement("h2", {}, "heading-h2 tag")]
   )
 );
 
