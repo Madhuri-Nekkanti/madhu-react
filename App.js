eb53aba5a961683411188b1e8f20c95862c50f96
Madhuri_N <madhuri.nekkanti@caprusit.com>
2025-08-29 18:03:00 +0530

    react functional component and added scripts

--- App.js
+++ App.js
@@ -1,34 +1,18 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
-// <div id="parent">
-//     <div id="child1">
-//       <h1>I'am h1 tag</h1>
-//       <h2>I'am h2 tag</h1>
-//     </div>
-//     <div id="child2">
-//       <h1>I'am h1 tag</h1>
-//       <h2>I'am h2 tag</h1>
-//     </div>
-// </div>
-// ReactElement(object) => HTML(Browser Understands)
+// React Element
+const heading = (
+  <h1 className='head' tabIndex='5'>
+    Namaste React using JSX
+  </h1>
+);
 
-// for more complex  structure elements
-
-// siblings - use Array
-
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1", key: "child1" }, [
-    React.createElement("h1", { key: "child1-h1" }, "heading-h1 tag"),
-    React.createElement("h2", { key: "child1-h2" }, "heading-h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2", key: "child2" }, [
-    React.createElement("h1", { key: "child2-h1" }, "heading-h1 tag"),
-    React.createElement("h2", { key: "child2-h2" }, "heading-h2 tag"),
-  ]),
-]);
-
-// JSX
+// React Functional Component
+const HeadingComponent = () => (
+  <div id='container'>
+    <h1 className='heading'>Namaste React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
